679b5a56e8beaa1ebd7671cecadb478d55631e81
qssseSammyink <gayerd05@gmail.com>
2025-11-16 05:36:40 +0000

    setup config for render and add ping command

--- src/slashCommands/General/ping.js
+++ src/slashCommands/General/ping.js
@@ -10,21 +10,14 @@ module.exports = {
      * @param {import('discord.js').Client} client
      * @param {import('discord.js').CommandInteraction} interaction
      */
-    run: async (client, interaction) => {
-      try {
-        const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+  async execute(client, interaction) {
+    const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
 
-        const latency = sent.createdTimestamp - interaction.createdTimestamp;
-        const apiLatency = Math.round(client.ws.ping);
+    const latency = sent.createdTimestamp - interaction.createdTimestamp;
+    const apiLatency = Math.round(client.ws.ping);
 
-        await interaction.editReply({
-          content: `Pong! 🏓\nLatency: ${latency}ms\nAPI Latency: ${apiLatency}ms`
-        });
-      } catch (error) {
-        console.error('Error executing ping command:', error);
-        if (!interaction.replied || interaction.deferred) {
-          await interaction.reply({ content: 'There was an error while executing this command.', ephemeral: true });
-        }
-      }
-    }
+    await interaction.editReply({
+      content:`Pong! 🏓\nLatency: ${latency}ms\nAPI Latency: ${apiLatency}ms`
+    });
+  },
 };
